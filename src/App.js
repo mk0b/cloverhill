bf2ddbd36432e5948739ccae483e716bf47b2f4d
Megan Katherine O'Brien <megankob@gmail.com>
2020-10-05 14:57:44 -0700

    Working on Nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 
+//take care of header code it's not in the nav component anymore.
+
 function App() {
   return (
     <Nav />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,11 @@ import { NavLink, HashRouter, Link } from 'react-router-dom';
 //nav bar 
 const Nav = (props) => (
     <HashRouter>
-        <nav>
+        <nav className="main-nav">
             <ul>
                 <li><NavLink to="/home">Home</NavLink></li>
+                <li><NavLink to="/services">Services</NavLink></li>
+                <li><NavLink to="/contactus">Conact Us</NavLink></li>
             </ul>
         </nav>
     </HashRouter>
